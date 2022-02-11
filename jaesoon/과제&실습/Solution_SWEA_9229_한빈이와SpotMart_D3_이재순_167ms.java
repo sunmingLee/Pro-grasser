b120b70789f5ffdd66facedf1566edbcfbb38702
dlwot <dlwot1112@gmail,com>
2022-02-11 14:02:29 +0900

    ~0211 과제&실습

--- jaesoon/과제&실습/Solution_SWEA_9229_한빈이와SpotMart_D3_이재순_167ms.java
+++ jaesoon/과제&실습/Solution_SWEA_9229_한빈이와SpotMart_D3_이재순_167ms.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_SWEA_9229_한빈이와SpotMart_D3_이재순_167ms {
+public class Solution_SWEA_9229_한빈이와SpotMart_D3_이재순_167ms { 
 	public static void main(String[] args) throws IOException {
 		int N, M, max;
 		int[] arr;
