a5f262477593347d7e1cf7b9466f52abaefc6000
seungyeon <seongyeon38@gmail.com>
2022-02-15 01:22:27 +0900

    Update Main_BOJ_5567_결혼식_S2_이승연_160ms.java

--- seungyeon/자료구조_트리, 연결리스트/Main_BOJ_5567_결혼식_S2_이승연_160ms.java
+++ seungyeon/자료구조_트리, 연결리스트/Main_BOJ_5567_결혼식_S2_이승연_160ms.java
@@ -5,27 +5,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_5567_결혼식_S2_이승연_160ms {
+public class Main_BOJ_5567_결혼식_S2_이승연_140ms {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine()); // 상근이의 동기의 수 
 		int M = Integer.parseInt(br.readLine()); // 리스트의 길이 
 		
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(); // 인접 리스트 
+		ArrayList<Integer>[] list = new ArrayList[N+1]; // 인접 리스트 
 		
 		StringTokenizer st;
 				
 		for(int i=0; i<=N; i++) {
-			list.add(new ArrayList<Integer>());
+			list[i] = new ArrayList<Integer>();
 		}
 		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int a1 = Integer.parseInt(st.nextToken());
 			int b1 = Integer.parseInt(st.nextToken());
-			list.get(a1).add(b1); // 양방향 (a1의 친구 b1)
-			list.get(b1).add(a1); // 양방향 (b1의 친구 a1)
+			list[a1].add(b1); // 양방향 (a1의 친구 b1)
+			list[b1].add(a1); // 양방향 (b1의 친구 a1)
 		}
 
 		boolean[] invite = new boolean[N+1]; // 초대할 친구 리스트 -> 인덱스가 학번 
@@ -40,9 +40,9 @@ public class Main_BOJ_5567_결혼식_S2_이승연_160ms {
 			for(int j=0; j<size; j++) {
 				int e = queue.poll(); // 하나씩 빼서 
 				
-				for(int k=0; k<list.get(e).size(); k++) { // 자기 자식의 노드들을 넣어줌 
-					queue.offer(list.get(e).get(k));
-					invite[list.get(e).get(k)] = true; // 초대할 친구 체크 
+				for(int k=0; k<list[e].size(); k++) { // 자기 자식의 노드들을 넣어줌 
+					queue.offer(list[e].get(k));
+					invite[list[e].get(k)] = true; // 초대할 친구 체크 
 				}
 			}
 		}
