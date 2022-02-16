797e510a22324e57f1d62b466dfb3c79b5acc470
seungyeon <seongyeon38@gmail.com>
2022-02-17 01:46:13 +0900

    Create Main_BOJ_9663_N_Queen_G5_이승연_15204ms.java

---
+++ seungyeon/완전탐색/Main_BOJ_9663_N_Queen_G5_이승연_15204ms.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BOJ_9663_N_Queen_G5_이승연_15204ms {
+	private static int N;
+	private static int[] arr;
+	private static int result;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine()); // (1 ≤ N < 15)
+		arr = new int[N]; // index: r, 해당 index의 값: c
+		result = 0;
+		
+		calcQueens(0);
+		
+		System.out.println(result);
+	}
+	
+	public static void calcQueens(int r) {
+		if(r == N) {
+			result++;
+			return;
+		}
+		
+		for(int c=0; c<N; c++) {
+			arr[r] = c;
+			boolean flag = true;
+			
+			for(int i=0; i<r; i++) {
+				if(arr[i] == c || Math.abs(r-i) == Math.abs(c-arr[i])) { // 열이 같은게 있으면 안됨. 행의 차 = 열의 차 -> 대각선에 존재
+					flag = false; 
+				}
+			}
+			
+			if(flag) { // c가 열이 될 수 있음. 
+				calcQueens(r+1); // 다음 행에 열 정하러감.
+			}
+		}
+	}
+}
