69827ac12a31ad3139c89a3a371c63bff7b16fe4
sdyoon94 <sdyoon94@gmail.com>
2022-02-20 02:42:05 +0900

    0219에 푼 IM대비문제

---
+++ seongdo/IM대비문제/Main_BOJ_17413_단어뒤집기2_S3_윤성도_180ms.java
@@ -0,0 +1,32 @@
+package com.ssafy.IM;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main_BOJ_17413_단어뒤집기2_S3_윤성도_180ms {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		ArrayDeque<Character> ad = new ArrayDeque<Character>();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0, size = input.length(); i < size; i++) {
+			switch(input.charAt(i)) {
+			case '<':
+				while(!ad.isEmpty()) sb.append(ad.pollLast());
+				sb.append('<');
+				while(input.charAt(++i) != '>') sb.append(input.charAt(i));
+				sb.append('>');
+				break;
+			case ' ':
+				while(!ad.isEmpty()) sb.append(ad.pollLast());
+				sb.append(' ');
+				break;
+			default:
+				ad.offerLast(input.charAt(i));
+			}
+		}
+		while(!ad.isEmpty()) sb.append(ad.pollLast());
+		System.out.println(sb.toString());
+	}
+}
