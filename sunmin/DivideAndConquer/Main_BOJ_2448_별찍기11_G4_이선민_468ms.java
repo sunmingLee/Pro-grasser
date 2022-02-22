ddda39b38b2d336243b20ae8ee629d5ae1e90ea8
sunmingLee <25thbam@gmail.com>
2022-02-22 12:31:58 +0900

    Update Main_BOJ_2448_별찍기11_G4_이선민_468ms.java
    파일명 변경

--- sunmin/DivideAndConquer/Main_BOJ_2448_별찍기11_G4_이선민_468ms.java
+++ sunmin/DivideAndConquer/Main_BOJ_2448_별찍기11_G4_이선민_468ms.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_BOJ_별찍기11_G4_이선민_468ms {
+public class Main_BOJ_2448_별찍기11_G4_이선민_468ms {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
