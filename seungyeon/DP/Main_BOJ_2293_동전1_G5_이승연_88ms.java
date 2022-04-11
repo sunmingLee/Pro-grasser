00c52f7a49c748d87a71a0f32e10653c3a2d17ef
seungyeon <seongyeon38@gmail.com>
2022-04-11 21:44:57 +0900

    Create Main_BOJ_2293_동전1_G5_이승연_88ms.java

---
+++ seungyeon/DP/Main_BOJ_2293_동전1_G5_이승연_88ms.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_2293_동전1_G5_이승연_88ms {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken()); // 동전의 종류 (1<=n<=100)
+		int k = Integer.parseInt(st.nextToken()); // 목표 가치 (1<=k<=10000)
+		
+		int[] value = new int[n]; // 동전의 가치 
+		
+		for(int i=0; i<n; i++) {
+			value[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] result = new int[k+1];
+		
+		for(int i=0; i<n; i++) {
+			int idx = value[i];
+			if(idx > k) continue;
+			result[idx]++; 
+			while(idx <= k) {
+				result[idx] += result[idx-value[i]];	
+				idx++;
+			}
+		}
+		
+		System.out.println(result[k]);
+	}
+}	
