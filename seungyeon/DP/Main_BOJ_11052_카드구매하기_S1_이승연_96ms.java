4b2f8bc32ca3141df2f8c74b67c6c9ee94873802
seungyeon <seongyeon38@gmail.com>
2022-04-12 17:38:09 +0900

    Update and rename Main_BOJ_11052_카드구매하기_S1_이승연_104ms.java to Main_BOJ_11052_카드구매하기_S1_이승연_96ms.java

--- seungyeon/DP/Main_BOJ_11052_카드구매하기_S1_이승연_96ms.java
+++ seungyeon/DP/Main_BOJ_11052_카드구매하기_S1_이승연_96ms.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_11052_카드구매하기_S1_이승연_104ms {
+public class Main_BOJ_11052_카드구매하기_S1_이승연_96ms {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -17,7 +17,7 @@ public class Main_BOJ_11052_카드구매하기_S1_이승연_104ms {
 		}
 		 
 		for(int i=1; i<N; i++) {
-			for(int j=0; j<i; j++) {
+			for(int j=0, size=i/2+1; j<size; j++) {
 				cost[i] = Math.max(cost[i], cost[j]+cost[i-j-1]);				
 			}
 		}
