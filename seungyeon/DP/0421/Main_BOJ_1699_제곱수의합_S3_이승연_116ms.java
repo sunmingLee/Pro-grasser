0b9f83d7019dd991c6a3783c439e6ea6adfd8c10
seungyeon <seongyeon38@gmail.com>
2022-04-21 21:02:27 +0900

    Update and rename Main_BOJ_1699_제곱수의합_S3_이승연2_116ms.java to Main_BOJ_1699_제곱수의합_S3_이승연_116ms.java

--- seungyeon/DP/0421/Main_BOJ_1699_제곱수의합_S3_이승연_116ms.java
+++ seungyeon/DP/0421/Main_BOJ_1699_제곱수의합_S3_이승연_116ms.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_BOJ_1699_제곱수의합_S3_이승연2_116ms {
+public class Main_BOJ_1699_제곱수의합_S3_이승연_116ms {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
