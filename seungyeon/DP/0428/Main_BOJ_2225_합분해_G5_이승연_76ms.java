c2db2435eec605589894fd4e300d3dfb5d32286f
seungyeon <seongyeon38@gmail.com>
2022-04-28 21:04:52 +0900

    Create Main_BOJ_2225_합분해_G5_이승연_76ms.java

---
+++ seungyeon/DP/0428/Main_BOJ_2225_합분해_G5_이승연_76ms.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_2225_합분해_G5_이승연_76ms {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken()); // 1<=N<=200
+		int K = Integer.parseInt(st.nextToken()); // 1<=K<=200
+		
+		// K개로 N 만들기 
+		int[][] dp = new int[K+1][N+1];
+		
+		for(int i=0; i<=K; i++) {
+			dp[i][0] = 1; // i개로 0 만들기 -> 1
+		}
+		
+		for(int i=1; i<=N; i++) {
+			dp[1][i] = 1; // 1개로 i 만들기 -> 1
+		}
+		
+		for(int i=1; i<=K; i++) { // i개로 j 만들기  
+			for(int j=1; j<=N; j++) {
+				dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000000;
+			}
+		}
+		
+		System.out.println(dp[K][N]);
+	}
+}
